129340d0bdf87528b1daa0c602595017ba7cf0a2
RenataNucci <renata.p.nucci@hotmail.com>
2022-06-21 18:36:23 -0300

    adc três constantes com o valor de um triangulo e adc formula de resolução

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const a = 2;
+/*const a = 2;
 const b = 3;
 
 let soma = a + b;
@@ -14,4 +14,25 @@ let divisao = a / b;
 console.log (divisao);
 
 let modulo = a % b;
-console.log (modulo);
+console.log (modulo); */
+
+let a = 65;
+let b = 100;
+let c = 15;
+
+let somaTriangulo = a + b + c;
+
+let triangulo = a > 0 && b > 0 && c > 0;
+
+if (triangulo) {
+    if (somaTriangulo===180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+} else {
+    console.log("Erro: ângulo inválido")
+};
+
+
+
